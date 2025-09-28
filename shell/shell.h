f71077bf06b0a569f2af0ed9881bc17153df899c
pablaofficeal <pavlogorosko56@gmail.com>
2025-09-28 11:10:27 +0200

    баг фикс

--- shell/shell.h
+++ shell/shell.h
@@ -1,4 +1,4 @@
-// shell.h - ДОБАВИТЬ ОБЪЯВЛЕНИЕ
+// shell.h - ДОБАВИТЬ новые объявления
 #ifndef SHELL_H
 #define SHELL_H
 
@@ -12,7 +12,7 @@ void cmd_clear();
 void cmd_echo(char *args);
 void cmd_version();
 void cmd_reboot();
-void cmd_shutdown();  // <-- ДОБАВИТЬ ЭТУ СТРОКУ
+void cmd_shutdown();
 void cmd_test(char *args);
 void cmd_keytest();
 void cmd_ls();
@@ -22,10 +22,12 @@ void cmd_rm(char *filename);
 void cmd_fsinfo();
 void cmd_df();
 void cmd_format(char *args);
+void cmd_create_test_files();
+
+// НОВЫЕ КОМАНДЫ - ДОБАВИТЬ ЭТИ СТРОКИ:
 void cmd_write(char *args);
 void cmd_info(char *filename);
 void cmd_rename(char *args);
 void cmd_space();
-void cmd_create_test_files();
 
 #endif
