61752421387a5b04d969660bf25930b280029271
pablaofficeal <pavlogorosko56@gmail.com>
2025-09-29 07:42:36 +0200

    я начел создавать игру 2048

---
+++ src/game/2048/field_16x16.h
@@ -0,0 +1,12 @@
+#ifndef FIELD_16X16_H
+#define FIELD_16X16_H
+
+#include "game_common.h"
+
+// Специфичные функции для 16x16
+int move_left_16x16(game_2048* game);
+int move_right_16x16(game_2048* game);
+int move_up_16x16(game_2048* game);
+int move_down_16x16(game_2048* game);
+
+#endif
