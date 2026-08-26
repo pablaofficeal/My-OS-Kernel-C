3b4e931f7af155a67e439f20a08bf7a2d309185e
pablaofficeal <pavlogorosko56@gmail.com>
2026-08-26 17:55:00 +0200

    start

--- src/boot/boot.c
+++ src/boot/boot.c
@@ -11,6 +11,9 @@
 #include "../kernel/syscall.h"
 #include "../kernel/kernel.h"
 
+__attribute__((used, section(".requests_start_marker")))
+static volatile LIMINE_REQUESTS_START_MARKER;
+
 __attribute__((used, section(".requests")))
 static volatile LIMINE_BASE_REVISION(2);
 
@@ -20,8 +23,11 @@ static volatile struct limine_framebuffer_request framebuffer_request = {
     .revision = 0
 };
 
-__attribute__((used, section(".requests_start_marker")))
-static volatile LIMINE_REQUESTS_START_MARKER;
+__attribute__((used, section(".requests")))
+static volatile struct limine_hhdm_request hhdm_request = {
+    .id = LIMINE_HHDM_REQUEST,
+    .revision = 0
+};
 
 __attribute__((used, section(".requests_end_marker")))
 static volatile LIMINE_REQUESTS_END_MARKER;
