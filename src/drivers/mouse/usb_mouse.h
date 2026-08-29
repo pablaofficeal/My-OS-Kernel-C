bffd94127f3629d01b8e9262af62325b49765bb8
pablaofficeal <pavlogorosko56@gmail.com>
2026-08-30 01:43:29 +0200

    stable release

--- src/drivers/mouse/usb_mouse.h
+++ src/drivers/mouse/usb_mouse.h
@@ -15,5 +15,4 @@ void usb_mouse_attach(uint16_t vendor_id, uint16_t product_id, uint8_t port);
 void usb_mouse_detach(void);
 void usb_mouse_report(const uint8_t *report, uint32_t length);
 void usb_mouse_poll(void);
-void usb_mouse_service_thread(void *arg);
 struct usb_mouse_info usb_mouse_get_info(void);
--- src/kernel/core/init.c
+++ src/kernel/core/init.c
@@ -5,7 +5,6 @@
 #include "../process/scheduler.h"
 #include "../process/process.h"
 #include "../../drivers/serial/serial.h"
-#include "../../drivers/mouse/usb_mouse.h"
 #include "../../userspace/userspace.h"
 #include "../../net/core/net_service.h"
 
@@ -42,7 +41,6 @@ void init_process_start(uint32_t detected_cpu_count){
     boot_diag_checkpoint(BOOT_STAGE_USERSPACE_RUN,
                          "init and desktop ready, starting scheduler");
 
-    scheduler_create_thread(usb_mouse_service_thread, 0, "usb-hid", 0, 0);
     scheduler_create_thread(userspace_input_thread, 0, "init-input", 1, 0);
     scheduler_create_thread(userspace_keyboard_thread, 0, "desktop-keyboard", 1, 0);
     scheduler_create_thread(userspace_log_thread, 0, "kernel-log", 3, 0);
@@ -53,4 +51,8 @@ void init_process_start(uint32_t detected_cpu_count){
     klog(KLOG_OK, "sched: init threads created, starting scheduler");
     serial_write_string("[SCHED] start\n");
     scheduler_start();
+
+    serial_write_string("[INIT] fallback run\n");
+    userspace_run();
+    kernel_panic("init process returned unexpectedly");
 }
